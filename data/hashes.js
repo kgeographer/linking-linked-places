35b219717d98e2f9fac34a0a41f88890c9366ab7
Karl Grossner <karl.geog@gmail.com>
2016-11-19 09:37:40 +0100

    Updates

---
+++ data/hashes.js
@@ -0,0 +1,26 @@
+window.tlConfig = {
+  "courier": {
+    "width1": "75%", "intUnit1":"DECADE", "intPixels1":50,
+    "width2": "25%", "intUnit2":"CENTURY", "intPixels2":120
+    },
+  "incanto-f": {
+    "width1": "75%", "intUnit1":"YEAR", "intPixels1":50,
+    "width2": "25%", "intUnit2":"DECADE", "intPixels2":120
+    },
+  "incanto-j": {
+    "width1": "75%", "intUnit1":"DECADE", "intPixels1":50,
+    "width2": "25%", "intUnit2":"CENTURY", "intPixels2":120
+    },
+  "roundabout": {
+    "width1": "75%", "intUnit1":"DAY", "intPixels1":50,
+    "width2": "25%", "intUnit2":"WEEK", "intPixels2":120
+    },
+  "vicarello": {
+    "width1": "75%", "intUnit1":"DECADE", "intPixels1":50,
+    "width2": "25%", "intUnit2":"CENTURY", "intPixels2":120
+    },
+  "xuanzang": {
+    "width1": "75%", "intUnit1":"YEAR", "intPixels1":50,
+    "width2": "25%", "intUnit2":"DECADE", "intPixels2":120
+    }
+}
