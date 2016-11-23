c486fd1e37e3b8603da0efe1b895fd9b8aaa5e62
Karl Grossner <karl.geog@gmail.com>
2016-11-23 10:11:56 +0100

    fixed elastic import

--- py/elastic.py
+++ py/elastic.py
@@ -8,26 +8,26 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 #projects = ["incanto", "vicarello", "courier", "xuanzang", "roundabout"]
-#projects = ["incanto", "vicarello", "xuanzang", "roundabout"]
-projects = ["courier"]
+projects = ["incanto", "vicarello", "xuanzang", "roundabout"]
+#projects = ["courier"]
 
 # put dataset in index
 # assumes mapping template
 for y in range(len(projects)):
     print(projects[y])
-    fin = codecs.open('_site/data/'+projects[y]+'.jsonl', 'r', 'utf8')
+    fin = codecs.open('../_site/data/'+projects[y]+'.jsonl', 'r', 'utf8')
     raw = fin.readlines()
     fin.close()    
     for x in range(len(raw)):
         doc = json.loads(raw[x])
         try:
-            res = es.index(index="linkedplaces", doc_type='place', id=doc['id'], body=raw[0])
-            #res = es.index(index="linkedplaces", doc_type='place', id=doc['id'], body=doc)
+            #res = es.index(index="linkedplaces", doc_type='place', id=doc['id'], body=raw[0])
+            res = es.index(index="linkedplaces", doc_type='place', id=doc['id'], body=doc)
             print(res['created'])
         except:
             print("error:", sys.exc_info()[0])
 
-es.indices.refresh(index="linkedplaces")
+#es.indices.refresh(index="linkedplaces")
 
 #res = es.search(index="linkedplaces", body={"query": {"match_all": {}}})
 #print("Got %d Hits:" % res['hits']['total'])
